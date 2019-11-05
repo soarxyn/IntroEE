6cd2650bdbffecb053bba23ad349a3c915a19296
Soarxyn <gamaraplays@gmail.com>
2019-11-05 19:33:04 -0200

    Framework: Added colors and organization. AI: Corrected prediction model

--- Manager.py
+++ Manager.py
@@ -1,52 +1,65 @@
-from chronometer import Chronometer
-from threading import Thread
-from time import sleep
-from pyfiglet import Figlet
+# Bibliotecas de Interface com Usuário
 from yaspin import yaspin
+from pyfiglet import Figlet
+from termcolor import colored
 from yaspin.spinners import Spinners
+
+# Bibliotecas para Paralelismo, Processamento e Comunicação
+from time import sleep
 from serial import Serial
+from threading import Thread
+from chronometer import Chronometer
 
+# Bibliotecas para AI e Tratamento / Exibição de Dados
 import numpy as numpy
-from sklearn.cluster import DBSCAN
 from sklearn import metrics 
+from sklearn.cluster import DBSCAN
 
-DEFAULT_STATION_BAUDRATE : int = 9600
-DEFAULT_SUPERVISOR_BAUDRATE : int = 4800
+DEFAULT_STATION_BAUDRATE : int = 9600           # Baudrate Padrão para Comunicação Serial ou Bluetooth com os Slaves das Estações.
+DEFAULT_SUPERVISOR_BAUDRATE : int = 4800        # Baudrate Padrão para Comunicação Serial ou Bluetooth com o Slave Supervisor.
 
-DEFAULT_STATION_PORT : int = 3
-DEFAULT_SUPERVISOR_PORT : int = 4
+DEFAULT_STATION_PORT : str = "/dev/ttyS3"       # Porta Padrão para Comunicação Serial ou Bluetooth com os Slaves das Estações.
+DEFAULT_SUPERVISOR_PORT : str = "/dev/ttyS4"    # Porta Padrão para Comunicação Serial ou Bluetooth com o Slave Supervisor.
 
-DATASET_FILE_PATH : str = "dbscan_dataset.csv"
+DATASET_FILE_PATH : str = "dataset.txt"     # Arquivo nos quais estão contidos os dados para feed no Algoritmo DBSCAN.
 
-isRunning : bool = False    # Flag de Execucao
+timerStation : Chronometer = Chronometer()  # Cronômetro para o Tempo gasto em cada Estação.
 
-lambdaAcc : float = 0.5     # Lambda distribuicao Poisson para Acidentes
-lambdaLck : float = 0.5     # Lambda distribuicao Poisson para Falta de Materiais
-lambdaMal : float = 0.5     # Lambda distribuicao Poisson para Equipamentos Defeituosos
+stationThread : Thread = NotImplemented     # Thread que executa a Await-For-Response do Arduino das Estações.
+controlThread : Thread = NotImplemented     # Thread que controla a Await-For-Response para parada, enquanto a `stationThread` está ocupada com o DBSCAN.
 
-timerStation : Chronometer = Chronometer()  # Timer para cronometrar o tempo nas estacoes
+isRunning : bool = False    # Indica o estado de execução do parâmetro para os laços das Threads.
 
-stationThread : Thread = NotImplemented
-controlThread : Thread = NotImplemented
+#   Limit = Average + STD * Threshold   ->  Limite para Suspeitar Anomalia.
+threshold : float = 1   # Threshold - Parâmetro que multiplica o corte de desvio médio.
+avg : float = 1         # Average   - Média dos valores de tempo do Dataset.
+std : float = 0.5       # STD       - Desvio Padrão dos valores de tempo do Dataset.
 
-threshold : float = 2
-average : float = 6.7
-std : float = 0.7
-eps : float = 0.3
+eps : float = 0.3   # Parâmetro Epsilon para o DBSCAN.
+
+lambdaAcc : float = 0.5     # Parâmetro Lambda da Distribuição de Poisson para Anomalias do tipo "Acidentes".
+lambdaLck : float = 0.5     # Parâmetro Lambda da Distribuição de Poisson para Anomalias do tipo "Falta de Materiais".
+lambdaMal : float = 0.5     # Parâmetro Lambda da Distribuição de Poisson para Anomalias do tipo "Equipamentos com Mau Funcionamento".
 
 def isOutlier(elapsedTime : float) -> bool:
-    dataset = numpy.genfromtxt(DATASET_FILE_PATH, delimiter = ',')
-    dbscan = DBSCAN(eps = eps, min_samples = 10).fit(dataset)
+    raw_data = open(DATASET_FILE_PATH, 'rt')
+    dataset = numpy.loadtxt(raw_data, delimiter=",")
 
-    core_samples = numpy.zeros_like(dbscan.labels_, dtype = bool)
-    core_samples[dbscan.core_sample_indices] = True 
+    insertion = numpy.append(dataset, [[elapsedTime, 0]], axis = 0)
+
+    dbscan = DBSCAN(eps = 0.3, min_samples = 10).fit(insertion)
     labels = dbscan.labels_
 
-    return labels[dataset.size//2 - 1] == -1
+    return labels[insertion.size//2 - 1] == -1
 
+def loadParameters():
+    return NotImplemented
 
-def t_StationThread(stationID : int, stationPort : int, stationBaudrate : int): 
-    stationPort : Serial = Serial(port = "/dev/ttyS" + str(stationPort), baudrate = stationBaudrate)
+def saveParameters():
+    return NotImplemented
+
+def t_StationThread(stationID : int, stationPort : str, stationBaudrate : int): 
+    stationPort : Serial = Serial(port = stationPort, baudrate = stationBaudrate)
 
     while isRunning:
         if timerStation.started:
@@ -73,36 +86,41 @@ def t_StationThread(stationID : int, stationPort : int, stationBaudrate : int):
                     pass        
 
 
-def t_ControlThread(stationID : int, stationPort : int, stationBaudrate : int):
+def t_ControlThread(stationID : int, stationPort : str, stationBaudrate : int):
     return NotImplemented
 
 if __name__ == "__main__":
     figlet = Figlet(font = "slant")
     print(figlet.renderText('Labrador'))
-    print("Welcome to the custom management system for Fabrica do Futuro - Escola Politecnica da Universidade de Sao Paulo")
-    print("Type 'help' for a list of available commands.")
+    print("Bem-vindo ao ", colored("Sistema de Gerenciamento e Aquisição de Dados", "cyan"), "Customizado para a", colored("Fábrica do Futuro - Escola Politécnica da Universidade de São Paulo", "yellow"))
+    print('')
 
-    with yaspin(Spinners.bouncingBall, text = "Carregando comunicacao com estacao", color = "blue") as loader:
+    with yaspin(Spinners.bouncingBall, text = "Carregando Comunicação com as Estações...", color = "blue") as loader:
         stationThread = Thread(target = t_StationThread, daemon = True, args = (0, DEFAULT_STATION_PORT, DEFAULT_STATION_BAUDRATE))
         sleep(3)
         loader.ok("> OK ")
 
-    with yaspin(Spinners.bouncingBall, text = "Carregando arquivos base", color = "yellow") as loader:
+    with yaspin(Spinners.bouncingBall, text = "Carregando os Arquivos de Configuração...", color = "yellow") as loader:
         sleep(3)
         loader.ok("> OK ")
 
+    print("\nEntre com", colored("help", "cyan", attrs = ["underline"]), "para ver comandos e outros funcionalidades.")
+
     isRunning = True
+    stationThread.start()
+
+    print('')
 
     while isRunning:
-        print("Enter a command: ", end = "")
+        print("Entre com um comando:", end = " ")
         command = input()
         
         if command == "quit":
             isRunning = False
         elif command == "help":
-            print("\nAvailable list of commands:")
-            print("     - quit: ends the main thread")
-            print("\n")
+            print("\nLista de comandos disponíveis:")
+            print("\t-", colored("help", "cyan"), ": Exibe esta informação, com os comandos disponíveis.")
+            print("\t-", colored("quit", "red"), ": Finaliza a execução do sistema.\n")
         else:
-            print("Command was not recognized.")
+            print(colored("Este comando não foi reconhecido!\n", "white", "on_red"))
     
